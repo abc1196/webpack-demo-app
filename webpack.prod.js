ac7350cfc94f233ce64a11881c0ca258a328e30a
abc1196 <alejob_11@hotmail.com>
2020-01-27 21:52:59 -0500

    Added optimizers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require('path');
 const commom = require('./webpack.common');
 const merge = require('webpack-merge');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(commom, {
     mode: 'production',
@@ -9,5 +13,32 @@ module.exports = merge(commom, {
         filename: '[name].[contentHash].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization: {
+        minimizer: [
+        new OptimizeCssAssetsPlugin(),
+        new TerserPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            minify: {
+              removeAttributeQuotes: true,
+              collapseWhitespace: true,
+              removeComments: true
+            }
+          })        ]
+    },
+    plugins: [   
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            MiniCssExtractPlugin.loader, //3. Inject styles into DOM
+            'css-loader', //2. Turns css into commonjs
+            'sass-loader'], //1. Turns sass into css
+        }
+      ],
+    },
 });
