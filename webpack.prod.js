8bed38713f0be126089608d0c150471b946e67eb
abc1196 <alejob_11@hotmail.com>
2020-01-27 21:05:37 -0500

    Added html-loader, file-loader and clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const commom = require('./webpack.common');
 const merge = require('webpack-merge');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = merge(commom, {
     mode: 'production',
     output: {
         filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [new CleanWebpackPlugin()]
 });
